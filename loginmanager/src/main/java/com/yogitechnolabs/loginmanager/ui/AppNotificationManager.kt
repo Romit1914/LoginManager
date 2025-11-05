1fe5a46df1dfe5a84e70f3f3c71faf8fbc4a688a
Romit D <romit.d@rumbum.co>
2025-11-05 14:05:41 +0530

    update

--- loginmanager/src/main/java/com/yogitechnolabs/loginmanager/ui/AppNotificationManager.kt
+++ loginmanager/src/main/java/com/yogitechnolabs/loginmanager/ui/AppNotificationManager.kt
@@ -5,6 +5,8 @@ import android.R
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.annotation.RequiresPermission
 import androidx.core.app.NotificationCompat
@@ -31,14 +33,24 @@ object AppNotificationManager {
         context: Context,
         title: String,
         message: String,
-        notificationId: Int = 1
+        notificationId: Int = 1,
+        imageResId: Int? = null,     // 👉 new param for image
+        smallIconRes: Int = R.drawable.ic_dialog_info // 👉 custom small icon
     ) {
         val builder = NotificationCompat.Builder(context, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_dialog_info)
+            .setSmallIcon(smallIconRes)
             .setContentTitle(title)
             .setContentText(message)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setAutoCancel(true)
+
+        // 👉 If image is provided, show Big Picture Style
+        imageResId?.let {
+            val bitmap: Bitmap = BitmapFactory.decodeResource(context.resources, it)
+            val style = NotificationCompat.BigPictureStyle()
+                .bigPicture(bitmap)
+            builder.setStyle(style)
+        }
 
         NotificationManagerCompat.from(context).notify(notificationId, builder.build())
     }
