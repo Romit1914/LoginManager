73508b30429b449c23e8e4d647eabb124831606a
Romit D <romit.d@rumbum.co>
2025-11-11 12:02:02 +0530

    update

--- loginmanager/build.gradle.kts
+++ loginmanager/build.gradle.kts
@@ -65,7 +65,7 @@ publishing {
             }
             groupId = "com.github.Romit1914"
             artifactId = "loginmanager"
-            version = "2.7.0"
+            version = "2.7.1"
         }
     }
 
