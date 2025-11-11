8e3c633efebe21ca073825ee2e936b9ee79d61d0
Romit D <romit.d@rumbum.co>
2025-11-11 11:08:48 +0530

    update

--- loginmanager/src/main/java/com/yogitechnolabs/loginmanager/MultiReelComponent.kt
+++ loginmanager/src/main/java/com/yogitechnolabs/loginmanager/MultiReelComponent.kt
@@ -1,9 +1,12 @@
 package com.yogitechnolabs.loginmanager
 
 import android.app.Activity
-import android.app.AlertDialog
 import android.graphics.Color
+import android.os.Build
+import android.view.View
 import android.view.ViewGroup
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.yogitechnolabs.loginmanager.model.ReelItem
@@ -14,14 +17,16 @@ object MultiReelComponent {
 
     private var reelAdapter: ReelAdapter? = null
     private var viewPager: ViewPager2? = null
+    private var isFullscreenEnabled = false
 
     fun show(
         activity: Activity,
         reels: List<ReelItem>,
         onAction: (action: ReelAction, reel: ReelItem) -> Unit
     ) {
-        // Agar already dikha rahe hain to remove karo pehle
         dismiss()
+
+        enableFullscreen(activity)
 
         val rootView = activity.findViewById<ViewGroup>(android.R.id.content)
 
@@ -38,48 +43,85 @@ object MultiReelComponent {
         reelAdapter = ReelAdapter(reels, onAction)
         viewPager!!.adapter = reelAdapter
 
-        // Page change callback
         viewPager!!.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-                pauseAllPlayers()
-                playPlayerAt(position)
+                pauseAllPlayersExcept(position)
             }
         })
 
-        // Add viewPager on top of root layout
         rootView.addView(viewPager)
 
-        // Auto play first reel
+        // Start playing first reel after layout ready
         viewPager!!.post {
-            playPlayerAt(0)
+            pauseAllPlayersExcept(0)
         }
     }
 
-    private fun pauseAllPlayers() {
+    private fun pauseAllPlayersExcept(positionToPlay: Int) {
         val recycler = viewPager?.getChildAt(0) as? RecyclerView ?: return
         for (i in 0 until recycler.childCount) {
             val holder = recycler.findViewHolderForAdapterPosition(i)
             if (holder is ReelAdapter.ReelViewHolder) {
-                holder.player?.pause()
+                if (holder.bindingAdapterPosition == positionToPlay) {
+                    holder.player?.play()
+                } else {
+                    holder.player?.pause()
+                }
             }
         }
     }
 
-    private fun playPlayerAt(position: Int) {
-        val recycler = viewPager?.getChildAt(0) as? RecyclerView ?: return
-        val holder = recycler.findViewHolderForAdapterPosition(position) as? ReelAdapter.ReelViewHolder
-        holder?.player?.play()
+    private fun enableFullscreen(activity: Activity) {
+        if (isFullscreenEnabled) return
+        isFullscreenEnabled = true
+
+        val window = activity.window
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.let {
+                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                it.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    )
+        }
     }
 
-    fun dismiss() {
-        pauseAllPlayers()
+    private fun disableFullscreen(activity: Activity) {
+        if (!isFullscreenEnabled) return
+        isFullscreenEnabled = false
+
+        val window = activity.window
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.show(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+        }
+    }
+
+    fun dismiss(activity: Activity? = null) {
+        pauseAllPlayersExcept(-1) // pause all players on dismiss
         viewPager?.let { vp ->
             val rootView = vp.parent as? ViewGroup
             rootView?.removeView(vp)
         }
         reelAdapter = null
         viewPager = null
+
+        activity?.let {
+            disableFullscreen(it)
+        }
     }
 }
-
