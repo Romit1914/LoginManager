f351551c0ad9888355f9dca2aced36992d9b1439
Romit D <romit.d@rumbum.co>
2025-11-12 15:49:56 +0530

    update

--- loginmanager/src/main/java/com/yogitechnolabs/loginmanager/model/ReelItem.kt
+++ loginmanager/src/main/java/com/yogitechnolabs/loginmanager/model/ReelItem.kt
@@ -1,8 +1,9 @@
 package com.yogitechnolabs.loginmanager.model
 
 data class ReelItem(
-    val videoUrl: String,
-    val description: String,
-    var isLiked: Boolean = false
+    val videoUrl                    :                    String,
+    val title                       :                    String ,
+    val description                 :                    String,
+    var isLiked                     :                    Boolean = false
 )
 
