b9b6b05a25ee74c5ee14a8e31e57dedd3ea473bd
Romit D <romit.d@rumbum.co>
2025-12-02 12:24:46 +0530

    salon component

--- loginmanager/src/main/java/com/yogitechnolabs/loginmanager/ContainerLayout.kt
+++ loginmanager/src/main/java/com/yogitechnolabs/loginmanager/ContainerLayout.kt
@@ -12,54 +12,43 @@ class ContainerLayout @JvmOverloads constructor(
 ) : LinearLayout(context, attrs) {
 
     private val manualMap = HashMap<String, Any>()
+    private val servicesList = mutableListOf<HashMap<String, Any>>() // for multiple services
 
-    init {
-        orientation = VERTICAL
-    }
-
-    // -------- PUBLIC FUNCTIONS -------- //
+    init { orientation = VERTICAL }
 
     fun add(key: String, value: Any): ContainerLayout {
         manualMap[key] = value
         return this
     }
 
+    fun addService(serviceName: String, price: Int): ContainerLayout {
+        servicesList.add(hashMapOf("serviceName" to serviceName, "price" to price))
+        return this
+    }
+
     fun build(): HashMap<String, Any> {
         val finalReq = HashMap<String, Any>()
-
-        // 1) Auto TextView read
         finalReq.putAll(collectTextValues(this))
-
-        // 2) Manual added values
         finalReq.putAll(manualMap)
-
+        if (servicesList.isNotEmpty()) finalReq["services"] = servicesList
+        finalReq["id"] = System.currentTimeMillis()
         return finalReq
     }
 
-    // -------- INTERNAL FUNCTIONS -------- //
-
     private fun collectTextValues(parent: ViewGroup): HashMap<String, Any> {
         val map = HashMap<String, Any>()
-
         for (i in 0 until parent.childCount) {
             val v = parent.getChildAt(i)
-
             if (v is TextView) {
-                val idName = try {
-                    resources.getResourceEntryName(v.id)
-                } catch (e: Exception) {
-                    ""
+                val idName = try { resources.getResourceEntryName(v.id) } catch (e: Exception) { "" }
+                if (idName.startsWith("txt_")) {
+                    val key = idName.substringAfter("_")
+                    map[key] = v.text.toString()
                 }
-
-                val key = idName.substringAfter("_", idName)
-                map[key] = v.text.toString()
             }
-
-            if (v is ViewGroup) {
-                map.putAll(collectTextValues(v))
-            }
+            if (v is ViewGroup) map.putAll(collectTextValues(v))
         }
-
         return map
     }
 }
+
