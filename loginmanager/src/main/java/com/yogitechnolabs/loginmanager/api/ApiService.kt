a34492f01fed0cccd22bc0753fa757c5b7b6e80e
Romit D <romit.d@rumbum.co>
2025-12-17 10:46:01 +0530

    change type

--- loginmanager/src/main/java/com/yogitechnolabs/loginmanager/api/ApiService.kt
+++ loginmanager/src/main/java/com/yogitechnolabs/loginmanager/api/ApiService.kt
@@ -78,7 +78,7 @@ interface ApiService {
     suspend fun getApi(
         @Url endpoint: String,
         @Header("X-API-SIGNATURE") signature: String? = null,
-        @Header("auth_token") authToken: String? = null,
+        @Header("TOKEN") authToken: String? = null,
     ): Response<String>
 
 
