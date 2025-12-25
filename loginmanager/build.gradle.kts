15b258a3099d76b4949358d31218498bc35bb01e
Romit D <romit.d@rumbum.co>
2025-12-25 12:40:10 +0530

    update version

--- loginmanager/build.gradle.kts
+++ loginmanager/build.gradle.kts
@@ -91,7 +91,7 @@ publishing {
             // ArtifactId is library name → will be used in dependency
             artifactId = "loginmanager"
             // Version → should match Git tag pushed to GitHub
-            version = "4.1.5"
+            version = "4.1.6"
         }
     }
 
